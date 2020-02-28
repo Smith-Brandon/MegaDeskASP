b7ed3d508f8d2144ced70d43cb5fd0779d96505a
rockenrooster <ran17022@byui.edu>
2020-02-28 12:54:46 -0600

    Added Search and Sort

--- Pages/DeskPages/Index.cshtml.cs
+++ Pages/DeskPages/Index.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using MegaDeskWebASP.Model;
-
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MegaDeskWebASP.Pages.DeskPages
 {
@@ -22,14 +22,52 @@ namespace MegaDeskWebASP.Pages.DeskPages
         public IList<DeskModel> DeskModel { get; set; }
         public List<DeskModel> Desk { get; set; }
 
+
+        
+        public string SearchString { get; set; }
+        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
+        public SelectList Names { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string CustName { get; set; }
+
+
+        public SelectList Date { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SortOrder { get; set; }
         public async Task OnGetAsync()
         {
             
         IQueryable<DeskModel> desks = from d in _context.DeskModel
                                      select d;
+            IQueryable<string> nameQuery = from m in _context.DeskModel
+                                            orderby m.Name
+                                            select m.Name;
 
 
 
+
+            if (!string.IsNullOrEmpty(CustName))
+            {
+                desks = desks.Where(s => s.Name == CustName);
+            }
+            if (SortOrder == "Oldest")
+            {
+                desks = desks.OrderBy(s => s.Date);
+            }
+            if (SortOrder == "Newest")
+            {
+                desks = desks.OrderByDescending(s => s.Date);
+            }
+            if (SortOrder == "Name Descending")
+            {
+                desks = desks.OrderByDescending(s => s.Name);
+            }
+            if (SortOrder == "Name Ascending")
+            {
+                desks = desks.OrderBy(s => s.Name);
+            }
+
+            Names = new SelectList(await nameQuery.Distinct().ToListAsync());
             DeskModel = await desks.ToListAsync();
             
         }
