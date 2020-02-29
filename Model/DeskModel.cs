32094c990b9583dbc804bec1f453989332d5c30b
gabrielcodingok <gabrielmemories@hotmail.com>
2020-02-29 19:50:39 -0300

    Updated Date Input

--- Model/DeskModel.cs
+++ Model/DeskModel.cs
@@ -36,6 +36,8 @@ namespace MegaDeskWebASP.Model
         public double Rush { get; set; }
         public double RushCost { get; set; }
         public double BasePrice = 200;
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public string Date { get; set; }
         public double TotalCost { get; set; }
 
