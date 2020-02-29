d61a54cdadab7f0f5fec2ffa00afd9f1422f56a0
gabrielcodingok <gabrielmemories@hotmail.com>
2020-02-28 22:11:57 -0300

    Added the Visual Clues

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,33 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+
+$(document).ready(function () {
+    $('#Desk_Material').on('change', function () {
+        $('#image1,#image2, #image3, #image4, #image5').hide();
+        if (this.value === 'Oak') {
+            $("#image1").show();
+        }
+        else if (this.value === 'Laminate') {
+            $("#image2").show();
+        }
+        else if (this.value === 'Veneer') {
+            $("#image3").show();
+        }
+        else if (this.value === 'Pine') {
+            $("#image4").show();
+        }
+        else if (this.value === 'Rosewood') {
+            $("#image5").show();
+        }
+        else {
+            $("#image1").hide();
+            $("#image2").hide();
+            $("#image3").hide();
+            $("#image4").hide();
+            $("#image5").hide();
+        }
+    })
+        .change();
+});
